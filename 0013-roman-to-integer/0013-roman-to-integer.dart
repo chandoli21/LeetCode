2d39c63de9a87ff58bc2f02effd21be76945f922
chandoli21 <68195563+chandoli21@users.noreply.github.com>
2025-03-12 09:56:40 +0900

    Time: 21 ms (21.95%), Space: 149.5 MB (64.48%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.dart
+++ 0013-roman-to-integer/0013-roman-to-integer.dart
@@ -1,15 +1,18 @@
 class Solution {
   int romanToInt(String s) {
+    // create a map that contains the roman numbers mapped to int
     Map<String, int> romanToInt = {
         'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
     };
 
     int total = 0;
 
+    // traverse the string and calculate the int values
     for (int i = 0; i < s.length; i++) {
         int curr = romanToInt[s[i]]!;
         int next = i + 1 < s.length ? romanToInt[s[i + 1]]! : 0;
 
+        // if the current value is less than the next value, subtract. else, add.
         if (curr < next) 
             total -= curr;
         else
