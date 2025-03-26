01046eb72bb927df1755cc4e07d7542df38129b1
chandoli21 <68195563+chandoli21@users.noreply.github.com>
2025-03-26 09:10:52 +0900

    Time: 1 ms (67.48%), Space: 151.9 MB (45.4%) - LeetHub

--- 0136-single-number/0136-single-number.dart
+++ 0136-single-number/0136-single-number.dart
@@ -1,10 +1,10 @@
 class Solution {
-  int singleNumber(List<int> nums) {
-    int result = 0;
-    for (int num in nums) {
-        result ^= num;
-    }
-    
-    return result;
-    }
+    int singleNumber(List<int> nums) {
+        int result = 0;
+        for (int num in nums) {
+            result ^= num;
+        }
+        
+        return result;
+        }
 }
