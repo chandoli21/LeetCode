67a1b306fb723cdf0ed3b017d65a7c93b88e6d09
chandoli21 <68195563+chandoli21@users.noreply.github.com>
2025-03-27 09:21:38 +0900

    Time: 1 ms (86.69%), Space: 155.7 MB (27.82%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.dart
@@ -0,0 +1,21 @@
+class Solution {
+    int majorityElement(List<int> nums) {
+    int count = 0;
+    int candidate = 0;
+
+    for (int num in nums) {
+        if (count == 0) {
+        candidate = num;
+        }
+        count += (num == candidate) ? 1 : -1;
+    }
+
+    return candidate;
+    }
+
+    void main() {
+    print(majorityElement([3, 2, 3])); // Output: 3
+    print(majorityElement([2, 2, 1, 1, 1, 2, 2])); // Output: 2
+    }
+
+}
