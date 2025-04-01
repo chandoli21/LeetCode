9e5a3991a9b52941a1395d0a5194498c9223d810
chandoli21 <68195563+chandoli21@users.noreply.github.com>
2025-04-01 09:23:09 +0900

    Time: 0 ms (100%), Space: 151.1 MB (25.29%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.dart
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *   int val;
+ *   ListNode? next;
+ *   ListNode([this.val = 0, this.next]);
+ * }
+ */
+class Solution {
+    ListNode? reverseList(ListNode? head) {
+        ListNode? prev = null;
+        ListNode? current = head;
+
+        while (current != null) {
+            ListNode? nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+
+        return prev;
+    }
+}
