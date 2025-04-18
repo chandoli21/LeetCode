a661d1ca6aa2637cbffc79ec2ff3c31022666bf5
chandoli21 <68195563+chandoli21@users.noreply.github.com>
2025-04-18 09:14:39 +0900

    Time: 0 ms (100%), Space: 151.5 MB (73.68%) - LeetHub

---
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.dart
@@ -0,0 +1,18 @@
+// class TreeNode {
+//    int val;
+//    TreeNode? left;
+//    TreeNode? right;
+//    TreeNode([this.val = 0, this.left, this.right]);
+// }
+
+class Solution {
+  TreeNode? searchBST(TreeNode? root, int val) {
+    if (root == null) return null;
+
+    if (root.val == val) return root;
+    else if (root.val > val) 
+        return searchBST(root.left, val);
+    else
+        return searchBST(root.right, val);
+  }
+}
