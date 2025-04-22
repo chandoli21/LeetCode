84c0bf4f602b914f45f85e2462e11c4c7864fd8c
chandoli21 <68195563+chandoli21@users.noreply.github.com>
2025-04-22 09:39:51 +0900

    Time: 0 ms (100%), Space: 150.6 MB (85.71%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.dart
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.dart
@@ -1,14 +1,18 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *   int val;
- *   TreeNode? left;
- *   TreeNode? right;
- *   TreeNode([this.val = 0, this.left, this.right]);
- * }
- */
+// class TreeNode {
+//    int val;
+//    TreeNode? left;
+//    TreeNode? right;
+//    TreeNode([this.val = 0, this.left, this.right]);
+// }
+
 class Solution {
   TreeNode? searchBST(TreeNode? root, int val) {
-    
+    if (root == null) return null;
+
+    if (root.val == val) return root;
+    else if (root.val > val) 
+        return searchBST(root.left, val);
+    else
+        return searchBST(root.right, val);
   }
 }
