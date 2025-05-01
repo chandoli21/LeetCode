71c462647cfc5940d9d943e107cb3e78b6049cb9
chandoli21 <68195563+chandoli21@users.noreply.github.com>
2025-05-01 09:04:55 +0900

    Time: 1 ms (83.93%), Space: 148.2 MB (76.79%) - LeetHub

---
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.dart
@@ -0,0 +1,6 @@
+class Solution {
+  List<bool> kidsWithCandies(List<int> candies, int extraCandies) {
+    int maxCandies = candies.reduce((a, b) => a > b ? a : b);
+    return candies.map((candy) => candy + extraCandies >= maxCandies).toList();
+  }
+}
