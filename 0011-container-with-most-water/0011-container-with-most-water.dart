e20332692fc7ecba6bbfc17525b05cfff6acee70
chandoli21 <68195563+chandoli21@users.noreply.github.com>
2025-05-12 09:14:23 +0900

    Time: 1 ms (100%), Space: 173.7 MB (51.52%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.dart
@@ -0,0 +1,25 @@
+class Solution {
+  int maxArea(List<int> height) {
+    int left = 0;
+    int right = height.length - 1;
+    int maxArea = 0;
+
+    while (left < right) {
+        int width = right - left;
+        int minHeight = height[left] < height[right] ? height[left] : height[right];
+        int currentArea = width * minHeight;
+
+        if (currentArea > maxArea) {
+            maxArea = currentArea;
+        }
+
+        if (height[left] < height[right]) {
+            left++;
+        }
+        else {
+            right--;
+        }        
+    }
+    return maxArea;
+  }
+}
